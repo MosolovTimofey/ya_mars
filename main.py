3603c8dc873a692712b0c4eb3c2fea5f78a9e636
Mosolov Timofey <timofey.mosolov@gmail.com>
2022-03-03 17:36:14 +0300

    задача 3

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, redirect
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
-from data.forms import RegisterForm
-from flask_login import LoginManager
+from data.forms import RegisterForm, LoginForm
+from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -15,6 +15,21 @@ login_manager.init_app(app)
 def load_user(id):
     db_sess = db_session.create_session()
     return db_sess.query(User).get(id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @app.route("/")
